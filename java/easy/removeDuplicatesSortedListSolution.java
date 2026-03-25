9e20b5946cab830295114254a511116fdd955148
CrisVOJ <cristopheroliverajimenez@gmail.com>
2026-03-25 12:22:07 -0400

    feat(remove duplicate sorted list java): Remove Duplicate Sorted List solution was added in Java

---
+++ java/easy/removeDuplicatesSortedListSolution.java
@@ -0,0 +1,40 @@
+/** Longest Palindromic Substring
+ * Problem link: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ * 
+ * Idea:
+ *  The main idea is to iterate through the linked list and, whenever the next node has the same value as the current one, skip it by updating the current node’s next pointer.
+ * 
+ * Complexity: O(n)
+ */
+
+package easy;
+
+import shared.ListNode;
+
+public class removeDuplicatesSortedListSolution {
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.next.val == current.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+
+    public static void showListNode(ListNode listNode) {
+        while (listNode.next != null) {
+            System.out.print(listNode.val + ", ");
+            listNode = listNode.next;
+        }
+        System.out.println(listNode.val);
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Result 1: "); showListNode(deleteDuplicates(new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3)))))));
+    }
+}
